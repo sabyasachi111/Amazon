d5f123d6fcfb8783a45e979aaa3d45c035e3c1f3
Sabyasachi <nayaksabyasachi06@gmail.com>
2024-10-10 15:14:02 +0530

    Register Functionality added

---
+++ Amazon/Controllers/ProductController.cs
@@ -0,0 +1,51 @@
+﻿using AmazonModel.Models;
+using DataLayer.DataContext;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Amazon.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly AmazonDbContext _context;
+        public ProductController(AmazonDbContext dbContext)
+        {
+            _context= dbContext;
+        }
+        [HttpGet]
+        public IActionResult GetAllProduct()
+        {
+
+            List<StoreProductsModel> products=_context.StoreProducts.ToList();
+            return View(products);
+        }
+        [HttpGet]
+        public IActionResult ProductDetails(int? ID) {
+            if (ID == null)
+            {
+                return BadRequest(ModelState);
+            }
+            
+                StoreProductsModel product = _context.StoreProducts.FirstOrDefault(x => x.ProductID == ID);
+            
+            return View(product);
+        }
+
+
+        [HttpGet]
+        public IActionResult AddProduct()
+        {
+            return View();  
+        }
+        [HttpPost]
+        public IActionResult AddProduct([FromForm] StoreProductsModel product) {
+
+            if(product == null)
+            {
+                return BadRequest(ModelState);
+            }
+             _context.StoreProducts.Add(product);
+            _context.SaveChanges();
+            return RedirectToAction("GetAllProduct");
+        }
+    }
+}
---
+++ DataLayer/Migrations/20241008104853_ColumnUpdated.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataLayer.Migrations
+{
+    public partial class ColumnUpdated : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "PartNumber",
+                table: "StoreProducts",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "PartNumber",
+                table: "StoreProducts",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
